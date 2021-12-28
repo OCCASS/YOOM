8fb26578322c3b7a379ab6e4fccb13f2860b198a
OCCCAS <pavlov.timur556@yandex.ru>
2021-12-28 18:26:36 +0300

    fix: create_map function fix, removed not used argument

--- map.py
+++ map.py
@@ -1,6 +1,8 @@
 from config import *
 
-def create_map(world_map, map):
+
+# Вайман Ангелина 28.12.2021. Создана функция create_map
+def create_map(map):
     for i, row in enumerate(map):
         for j, el in enumerate(row):
             if el == WALL_CHARS[0] or el == WALL_CHARS[1]:
