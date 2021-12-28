ff5a7e97b9dcef284e51a891563af05fd6711e44
OCCCAS <pavlov.timur556@yandex.ru>
2021-12-28 18:25:35 +0300

    fix: ray_casting function bug fix and function refactoring

--- ray_casting.py
+++ ray_casting.py
@@ -1,19 +1,33 @@
-import pygame
-from config import *
+from config import HALF_FOV, RAYS_AMOUNT, DELTA_ANGLE, WORLD_MAP, TILE, MAX_RAY_DISTANCE
 import math
 
+"""
+Вайман Ангелина 28.12.2021. Создана функция ray_casting.
 
-def ray_casting(screen, player_pos, player_angle):
-    cur_angle = player_angle - (FOV / 2)
+Павлов Тимур 28.12.2021. Доработка функции ray_casting
+"""
+
+
+def ray_casting(player_pos, player_angle):
+    cur_angle = player_angle - HALF_FOV
+
     points = []
     for ray in range(RAYS_AMOUNT):
-        sin_a = math.sin(cur_angle)
-        cos_a = math.cos(cur_angle)
-        for depth in range(MAX_DEPTH):
-            x = player_pos[0] + depth * cos_a
-            y = player_pos[1] + depth * sin_a
-            points.append((x, y))
-            if (x // TILE * TILE, y // TILE * TILE) in WORLD_MAP:
-                break
+        collision_pos = collide_nearest_wall(player_pos, cur_angle)
+        points.append(collision_pos)
+
         cur_angle += DELTA_ANGLE
-        return points
+
+    return points
+
+
+def collide_nearest_wall(start_pos: tuple, angel: float) -> tuple[float, float]:
+    start_x, start_y = start_pos
+    cos_a, sin_a = math.cos(angel), math.sin(angel)
+
+    for distance in range(MAX_RAY_DISTANCE):
+        x = start_x + distance * cos_a
+        y = start_y + distance * sin_a
+
+        if (x // TILE * TILE, y // TILE * TILE) in WORLD_MAP:
+            return x, y
