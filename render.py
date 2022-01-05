75d9b745e0c00d16a751d8bad345fa9a4d8d8799
YoungCucumber <92947429+youngcucumber@users.noreply.github.com>
2022-01-05 12:12:13 +0300

    fix: unuseful code was removes

--- render.py
+++ render.py
@@ -8,11 +8,12 @@ from load_image import load_image
 Вайман Ангелина:
 02.01.2022. Создан класс Render с использованием алгоритма Павлова Тимура
 03.01.2022. Создана функция _draw_minimap, _draw_sky
-04.01.2022. Доработано фунукция _draw_walls и добавлены текстуры
+04.01.2022. Доработана фунукция _draw_walls и добавлены текстуры
 """
 
 sky_texture = load_image(SKY_TEXTURE)
 wall_texture = load_image(WALL_TEXTURE)
+floor_texture = load_image(FLOOR_TEXTURE)
 
 
 class Render:
@@ -26,15 +27,6 @@ class Render:
         self._draw_floor()
         self._draw_walls()
         self._draw_minimap()
-
-    def _draw_player(self):
-        player_pos = self.player.pos
-        pygame.draw.circle(self.screen, BLACK, (player_pos.x, player_pos.y), 5)
-        hits = ray_casting(player_pos, self.player.direction)
-
-        for hit in hits:
-            hit_point = hit.point
-            pygame.draw.line(self.screen, WHITE, player_pos, hit_point)
 
     def _draw_floor(self):
         pygame.draw.rect(self.screen, BRICK, (0, HALF_SCREEN_HEIGHT, SCREEN_WIDTH, HALF_SCREEN_HEIGHT))
@@ -58,20 +50,6 @@ class Render:
             wall = wall_texture.subsurface(offset * TEXTURE_SCALE, 0, TEXTURE_SCALE, TEXTURE_HEIGHT)
             wall = pygame.transform.scale(wall, (SCALE, projection_height))
             self.screen.blit(wall, (hit_index * SCALE, HALF_SCREEN_HEIGHT - projection_height // 2))
-
-    def _draw_map(self):
-        for row_index, row in enumerate(MAP):
-            for char_index, char in enumerate(row):
-                if char in WALL_CHARS:
-                    if char == WALL_CHARS[0]:
-                        color = ORANGE
-                    elif char == WALL_CHARS[1]:
-                        color = PURPLE
-                    else:
-                        color = BLACK
-
-                    x, y = char_index * TILE, row_index * TILE
-                    pygame.draw.rect(self.screen, color, (x, y, TILE, TILE))
 
     def _draw_minimap(self):
         self.screen_map.fill(BLACK)
