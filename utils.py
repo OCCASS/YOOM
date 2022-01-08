44e894e0fc736889f346fe885080c9dfa430659f
OCCCAS <pavlov.timur556@yandex.ru>
2022-01-08 16:20:25 +0300

    feat: created new function

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
 import numba
 import math
+from config import TILE
 
 """
-Павлов Тимур 08.01.2022. Создана функция get_distance
+Павлов Тимур 08.01.2022. Создана функция get_distance, world_pos2cell
 """
 
 
 @numba.njit(fastmath=True, cache=True)
 def get_distance(x0, y0, x1, y1):
     return math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2)
+
+
+@numba.njit(fastmath=True, cache=True)
+def world_pos2cell(x, y):
+    return x // TILE, y // TILE
