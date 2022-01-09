4de08beaf31a6b6dd5324dd3b5d8eba26ff50fb8
YoungCucumber <angelaaav@mail.ru>
2022-01-09 04:16:27 +0300

    fix: constants fixed

--- menu.py
+++ menu.py
@@ -33,8 +33,8 @@ def button(screen, name, color, rect_pos, width, height):
 def show_info(screen, player):
     health = 'Health   ' + str(player.health)
     ammo = 'Ammo   ' + str(player.weapons[player.current_gun_index].ammo)
-    button(screen, health, 'red', (10, SCREEN_HEIGHT - 160), 140, 70)
-    button(screen, ammo, 'blue', (10, SCREEN_HEIGHT - 90), 140, 70)
+    button(screen, health, RED, (10, SCREEN_HEIGHT - 160), 140, 70)
+    button(screen, ammo, BLUE, (10, SCREEN_HEIGHT - 90), 140, 70)
 
 
 def show_game_over(screen):
@@ -60,7 +60,7 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            self.screen.blit(background, (0, 0),
+            self.screen.blit(background, MENU_BACKGROUND_POS,
                              (self.x % SCREEN_WIDTH, HALF_SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT))
             self.x += 1
             self._logo()
@@ -72,9 +72,9 @@ class Menu:
             self.clock.tick(20)
 
     def _logo(self):
-        color = random.randint(0, 50)
+        color = random.randint(LOGO_COLOR[0], LOGO_COLOR[1])
         logo = logo_font.render(LOGO, True, (color, color, color))
-        self.screen.blit(logo, (HALF_SCREEN_WIDTH - 270, 0))
+        self.screen.blit(logo, LOGO_POS)
 
     def _create_buttons(self):
         pass
@@ -92,13 +92,14 @@ class MainMenu(Menu):
         self.levels_class = Levels(self.screen, self.clock)
 
     def _create_buttons(self):
-        self.btn_levels, self.levels = button(self.screen, LEVELS_NAME, BLACK, (HALF_SCREEN_WIDTH - 140, 200),
+        self.btn_levels, self.levels = button(self.screen, LEVELS_NAME, BLACK, BTN_LEVELS_POS,
                                               MENU_BTN_SIZE[0], MENU_BTN_SIZE[1])
-        self.btn_arcade, self.arcade = button(self.screen, ARCADE_NAME, BLACK, (HALF_SCREEN_WIDTH - 140, 320),
+        self.btn_arcade, self.arcade = button(self.screen, ARCADE_NAME, BLACK, BTN_ARCADE_POS,
                                               MENU_BTN_SIZE[0], MENU_BTN_SIZE[1])
-        self.btn_settings, self.settings = button(self.screen, SETTINGS_NAME, BLACK, (HALF_SCREEN_WIDTH - 185, 440),
+        self.btn_settings, self.settings = button(self.screen, SETTINGS_NAME, BLACK, BTN_SETTINGS_POS,
                                                   MENU_BTN_SIZE[0], MENU_BTN_SIZE[1])
-        self.btn_exit, self.exit = button(self.screen, EXIT_NAME, BLACK, (50, 500), 150, 50)
+        self.btn_exit, self.exit = button(self.screen, EXIT_NAME, BLACK, BTN_EXIT_BACK_POS, BTN_EXIT_BACK_SIZE[0],
+                                          BTN_EXIT_BACK_SIZE[1])
 
     def _mouse_operations(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -110,7 +111,7 @@ class MainMenu(Menu):
 
     def _btn_levels_check(self, mouse_pos, mouse_click):
         if self.btn_levels.collidepoint(mouse_pos):
-            button(self.screen, LEVELS_NAME, WHITE, (HALF_SCREEN_WIDTH - 140, 200), MENU_BTN_SIZE[0], MENU_BTN_SIZE[1])
+            button(self.screen, LEVELS_NAME, WHITE, BTN_LEVELS_POS, MENU_BTN_SIZE[0], MENU_BTN_SIZE[1])
             if mouse_click[0]:
                 self.chosen_level = self.levels_class.run()
                 if self.chosen_level:
@@ -118,16 +119,16 @@ class MainMenu(Menu):
 
     def _btn_arcade_check(self, mouse_pos, mouse_click):
         if self.btn_arcade.collidepoint(mouse_pos):
-            button(self.screen, ARCADE_NAME, WHITE, (HALF_SCREEN_WIDTH - 140, 320), MENU_BTN_SIZE[0], MENU_BTN_SIZE[1])
+            button(self.screen, ARCADE_NAME, WHITE, BTN_ARCADE_POS, MENU_BTN_SIZE[0], MENU_BTN_SIZE[1])
 
     def _btn_settings_check(self, mouse_pos, mouse_click):
         if self.btn_settings.collidepoint(mouse_pos):
-            button(self.screen, SETTINGS_NAME, WHITE, (HALF_SCREEN_WIDTH - 185, 440), MENU_BTN_SIZE[0],
+            button(self.screen, SETTINGS_NAME, WHITE, BTN_SETTINGS_POS, MENU_BTN_SIZE[0],
                    MENU_BTN_SIZE[1])
 
     def _btn_exit_check(self, mouse_pos, mouse_click):
         if self.btn_exit.collidepoint(mouse_pos):
-            button(self.screen, EXIT_NAME, WHITE, (50, 500), 150, 50)
+            button(self.screen, EXIT_NAME, WHITE, BTN_EXIT_BACK_POS, BTN_EXIT_BACK_SIZE[0], BTN_EXIT_BACK_SIZE[1])
             if mouse_click[0]:
                 pygame.quit()
                 sys.exit()
@@ -143,22 +144,18 @@ class Levels(Menu):
             return self.chosen_level
 
     def _create_buttons(self):
-        self.btn_level_1, self.level_1 = button(self.screen, LEVEL_1_NAME, BLACK, (HALF_SCREEN_WIDTH - 140, 200),
-                                                LEVEL_BTN_SIZE[0],
-                                                LEVEL_BTN_SIZE[1])
-        self.btn_level_2, self.level_2 = button(self.screen, LEVEL_2_NAME, BLACK, (HALF_SCREEN_WIDTH - 140, 270),
-                                                LEVEL_BTN_SIZE[0],
-                                                LEVEL_BTN_SIZE[1])
-        self.btn_level_3, self.level_3 = button(self.screen, LEVEL_3_NAME, BLACK, (HALF_SCREEN_WIDTH - 140, 340),
-                                                LEVEL_BTN_SIZE[0],
-                                                LEVEL_BTN_SIZE[1])
-        self.btn_level_4, self.level_4 = button(self.screen, LEVEL_4_NAME, BLACK, (HALF_SCREEN_WIDTH - 140, 410),
-                                                LEVEL_BTN_SIZE[0],
-                                                LEVEL_BTN_SIZE[1])
-        self.btn_level_5, self.level_5 = button(self.screen, LEVEL_5_NAME, BLACK, (HALF_SCREEN_WIDTH - 140, 480),
-                                                LEVEL_BTN_SIZE[0],
-                                                LEVEL_BTN_SIZE[1])
-        self.btn_menu_back, self.menu_back = button(self.screen, BACK_NAME, BLACK, (50, 500), 150, 50)
+        self.btn_level_1, self.level_1 = button(self.screen, LEVEL_1_NAME, BLACK, BTN_LEVEL_1_POS,
+                                                LEVEL_BTN_SIZE[0], LEVEL_BTN_SIZE[1])
+        self.btn_level_2, self.level_2 = button(self.screen, LEVEL_2_NAME, BLACK, BTN_LEVEL_2_POS,
+                                                LEVEL_BTN_SIZE[0], LEVEL_BTN_SIZE[1])
+        self.btn_level_3, self.level_3 = button(self.screen, LEVEL_3_NAME, BLACK, BTN_LEVEL_3_POS,
+                                                LEVEL_BTN_SIZE[0], LEVEL_BTN_SIZE[1])
+        self.btn_level_4, self.level_4 = button(self.screen, LEVEL_4_NAME, BLACK, BTN_LEVEL_4_POS,
+                                                LEVEL_BTN_SIZE[0], LEVEL_BTN_SIZE[1])
+        self.btn_level_5, self.level_5 = button(self.screen, LEVEL_5_NAME, BLACK, BTN_LEVEL_5_POS,
+                                                LEVEL_BTN_SIZE[0], LEVEL_BTN_SIZE[1])
+        self.btn_menu_back, self.menu_back = button(self.screen, BACK_NAME, BLACK, BTN_EXIT_BACK_POS,
+                                                    BTN_EXIT_BACK_SIZE[0], BTN_EXIT_BACK_SIZE[1])
 
     def _mouse_operations(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -175,41 +172,41 @@ class Levels(Menu):
 
     def _btn_level_1_check(self, mouse_pos, mouse_click):
         if self.btn_level_1.collidepoint(mouse_pos):
-            button(self.screen, LEVEL_1_NAME, WHITE, (HALF_SCREEN_WIDTH - 140, 200), LEVEL_BTN_SIZE[0],
+            button(self.screen, LEVEL_1_NAME, WHITE, BTN_LEVEL_1_POS, LEVEL_BTN_SIZE[0],
                    LEVEL_BTN_SIZE[1])
             if mouse_click[0]:
                 return load_level(LEVEL_1)
 
     def _btn_level_2_check(self, mouse_pos, mouse_click):
         if self.btn_level_2.collidepoint(mouse_pos):
-            button(self.screen, LEVEL_2_NAME, WHITE, (HALF_SCREEN_WIDTH - 140, 270), LEVEL_BTN_SIZE[0],
+            button(self.screen, LEVEL_2_NAME, WHITE, BTN_LEVEL_2_POS, LEVEL_BTN_SIZE[0],
                    LEVEL_BTN_SIZE[1])
             if mouse_click[0]:
                 return load_level(LEVEL_2)
 
     def _btn_level_3_check(self, mouse_pos, mouse_click):
         if self.btn_level_3.collidepoint(mouse_pos):
-            button(self.screen, LEVEL_3_NAME, WHITE, (HALF_SCREEN_WIDTH - 140, 340), LEVEL_BTN_SIZE[0],
+            button(self.screen, LEVEL_3_NAME, WHITE, BTN_LEVEL_3_POS, LEVEL_BTN_SIZE[0],
                    LEVEL_BTN_SIZE[1])
             if mouse_click[0]:
                 return load_level(LEVEL_3)
 
     def _btn_level_4_check(self, mouse_pos, mouse_click):
         if self.btn_level_4.collidepoint(mouse_pos):
-            button(self.screen, LEVEL_4_NAME, WHITE, (HALF_SCREEN_WIDTH - 140, 410), LEVEL_BTN_SIZE[0],
+            button(self.screen, LEVEL_4_NAME, WHITE, BTN_LEVEL_4_POS, LEVEL_BTN_SIZE[0],
                    LEVEL_BTN_SIZE[1])
             if mouse_click[0]:
                 return load_level(LEVEL_4)
 
     def _btn_level_5_check(self, mouse_pos, mouse_click):
         if self.btn_level_5.collidepoint(mouse_pos):
-            button(self.screen, LEVEL_5_NAME, WHITE, (HALF_SCREEN_WIDTH - 140, 480), LEVEL_BTN_SIZE[0],
+            button(self.screen, LEVEL_5_NAME, WHITE, BTN_LEVEL_5_POS, LEVEL_BTN_SIZE[0],
                    LEVEL_BTN_SIZE[1])
             if mouse_click[0]:
                 return load_level(LEVEL_5)
 
     def _btn_back_check(self, mouse_pos, mouse_click):
         if self.btn_menu_back.collidepoint(mouse_pos):
-            button(self.screen, BACK_NAME, WHITE, (50, 500), 150, 50)
+            button(self.screen, BACK_NAME, WHITE, BTN_EXIT_BACK_POS, BTN_EXIT_BACK_SIZE[0], BTN_EXIT_BACK_SIZE[1])
             if mouse_click[0]:
                 self.levels_run = False
